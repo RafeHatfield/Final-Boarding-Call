15e5176af592a16b96598a00a3390d2a47a85167
Rafe Hatfield <rafe.hatfield@suite101.com>
2012-01-13 09:58:07 -0800

    bumped version number; completed generator for creating migration but having namespace issue, next update needs to rename the model to stop confusion

--- lib/finalboardingcall.rb
+++ lib/finalboardingcall.rb
@@ -8,46 +8,44 @@ module Rack
 		end
 
 	  def call(env)
-
-			[200, {"Content-Type" => "text/html"}, ["I am a grateful grapefruit"]]
-
-			# status, headers, response = @app.call(env)
-			# 
-			# logDir = Rails.root.join('log')
-			# logFile = "check404.log"
-			# # 
-			# # File.open(logDir.join(logFile), 'a') do |file|
-			# # 	file.puts("------------------")
-			# # 	file.puts(status)
-			# # 	file.puts(headers)
-			# # 	file.puts(env['REQUEST_URI'])
-			# # 	file.puts("------------------")
-			# # end
-			# 
-			# if status == 404 && goto = check_redirect(env['REQUEST_URI'])
-			# 
-			# 		File.open(logDir.join(logFile), 'a') do |file|
-			# 			file.puts("------------------")
-			# 			file.puts("REDIRECTING!")
-			# 			# file.puts(env.inspect)
-			# 			file.puts(status)
-			# 			file.puts(headers)
-			# 			file.puts(env['REQUEST_URI'])
-			# 			file.puts(goto.new_url)
-			# 			file.puts("------------------")
-			# 		end
-			# 
-			# 		# check redirect
-			# 		[goto.redirect_type, {"Location"=> goto.new_url}, ['Hasta La Vista']]
-			# 
-			# else
-			# 	[status, headers, response]
-			# end
+		
+		status, headers, response = @app.call(env)
+		
+		logDir = Rails.root.join('log')
+		logFile = "finalboardingcall.log"
+		
+		File.open(logDir.join(logFile), 'a') do |file|
+			file.puts("------------------")
+			file.puts(status)
+			file.puts(headers)
+			file.puts(env['REQUEST_URI'])
+			file.puts("------------------")
 		end
+		
+		[200, {"Content-Type" => "text/html"}, ["I am a grateful grapefruit"]]
 
-		# def check_redirect(url)
-		# 	check_url = Redirect.where(['old_url = ?', url]).first
+		# if status == 404 && goto = check_redirect(env['REQUEST_URI'])
+		# 	
+		# 		File.open(logDir.join(logFile), 'a') do |file|
+		# 			file.puts("------------------")
+		# 			file.puts("REDIRECTING!")
+		# 			# file.puts(env.inspect)
+		# 			file.puts(status)
+		# 			file.puts(headers)
+		# 			file.puts(env['REQUEST_URI'])
+		# 			file.puts(goto.new_url)
+		# 			file.puts("------------------")
+		# 		end
+		# 
+		# 		# check redirect
+		# 		[goto.redirect_type, {"Location"=> goto.new_url}, ['Hasta La Vista']]
+		# 
+		# else
+		# 	[status, headers, response]
 		# end
 
+		def check_redirect(url)
+			check_url = Finalboardingcall.where(['old_url = ?', url]).first
+		end
 	end
 end
